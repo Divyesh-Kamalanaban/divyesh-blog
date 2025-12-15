4d99d55a6a3a331e801e19578ee565585c40965c
Divyesh-Kamalanaban <divyeshkamalanaban@gmail.com>
2025-12-15 23:29:49 +0530

    Initial site setup

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Divyesh Kamalanaban';
+export const SITE_DESCRIPTION = 'divyesh.is-cool.dev will contain devlogs, writing about new discoveries and explorations into the fields of embedded hardware, Linux development, DIY tech as a full stack developer (student currently).';
