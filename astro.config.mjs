ddffd33b05d46233e4c26611d707858b2606013a
Divyesh-Kamalanaban <divyeshkamalanaban@gmail.com>
2025-12-16 09:15:07 +0530

    Added mermaid rendering and font optimisations! Changes to be committed: 	modified:   astro.config.mjs 	modified:   package-lock.json 	modified:   package.json 	deleted:    public/favicon.svg 	deleted:    public/fonts/Merriweather-Italic.ttf 	deleted:    public/fonts/Merriweather-Variable.ttf 	new file:   public/fonts/Merriweather-Variable.woff2 	modified:   src/styles/global.css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,40 @@
 // @ts-check
-
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import tailwindcss from '@tailwindcss/vite';
+import mermaid from 'astro-mermaid';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://divyesh.is-cool.dev',
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [mdx(), sitemap(), react(), mermaid({
+  // Default theme: 'default', 'dark', 'forest', 'neutral', 'base'
+  theme: 'forest',
+  
+  // Enable automatic theme switching based on data-theme attribute
+  autoTheme: true,
+  
+  // Additional mermaid configuration
+  mermaidConfig: {
+    flowchart: {
+      curve: 'basis'
+    }
+  },
+  
+  // Register icon packs for use in diagrams
+  iconPacks: [
+    {
+      name: 'logos',
+      loader: () => fetch('https://unpkg.com/@iconify-json/logos@1/icons.json').then(res => res.json())
+    },
+    {
+      name: 'iconoir',
+      loader: () => fetch('https://unpkg.com/@iconify-json/iconoir@1/icons.json').then(res => res.json())
+    }
+  ]
+})],
 
   vite: {
     plugins: [tailwindcss()],
